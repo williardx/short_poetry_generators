86cc598c9a9d8ceca3f4fea82eaea2ed4af9f895
wdoenlen <wdoenlen@gmail.com>
2013-05-19 13:28:50 -0400

    first commit

---
+++ stopwords.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import random
+import time
+
+linkers1 = ["if", "because", "and", "or", "let", "then", "when", "but"]
+linkers2 = ["if", "because", "and", "or", "let", "then", "failed", "loved"]
+subjects = ["I", "you", "we"]
+objects = ["her", "him", "I", "us"]
+when = ["day", "month", "year", "moment"]
+desc = ["same", "next", "longest", "previous", "forgotten"]
+det = ["That", "The"]
+1
+while True:
+    
+    link1 = random.choice(linkers1)
+    link2 = random.choice(linkers2)
+    count = 0
+    print "\n" + random.choice(det) + " " + random.choice(desc) + " " + random.choice(when) + "\n"
+    for i in range(5):
+        subj = random.choice(subjects)
+        obj = random.choice(objects)     
+        print count*".." + link1 + " " + subj + " " + link2 + " " + obj
+        time.sleep(1.2)
+        count += 1
